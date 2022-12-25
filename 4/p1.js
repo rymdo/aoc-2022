dd63983ff45275678751671fcda8547b1d89d775
Niklas Wigertz <niklas.wigertz.danielsson@gmail.com>
2022-12-25 13:16:29 +0100

    d4p1

---
+++ 4/p1.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+
+function getInputLines() {
+  const input = fs.readFileSync("./input").toString()
+  return input.split(/\r?\n/)
+}
+
+function mapSectionRange(range) {
+  const limits = range.split("-")
+  const start = Number(limits[0]);
+  const end = Number(limits[1]);
+  const result = [...Array(end - start + 1).keys()].map(x => x + start);
+  return result
+}
+
+function getSectionPairs(lines) {
+  const result = []
+  for (const line of lines) {
+    const sections = line.split(",")
+    result.push({
+      section1: mapSectionRange(sections[0]),
+      section2: mapSectionRange(sections[1])
+    })
+  }
+  return result
+}
+
+function isContained(sectionPair) {
+  const {
+    section1,
+    section2
+  } = sectionPair
+  const intersection = section1.filter(value => section2.includes(value));
+  if (JSON.stringify(section1) == JSON.stringify(intersection)) {
+    return true
+  }
+  if (JSON.stringify(section2) == JSON.stringify(intersection)) {
+    return true
+  }
+  return false
+}
+
+const inputLines = getInputLines()
+const sectionPairs = getSectionPairs(inputLines)
+
+let containedCount = 0
+for (const sectionPair of sectionPairs) {
+  const contained = isContained(sectionPair)
+  if (contained) {
+    containedCount += 1
+  }
+}
+
+console.log(`day4 - part1 - count: ${containedCount}`)
